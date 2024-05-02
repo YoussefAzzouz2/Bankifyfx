40dda9897dd1e94c83853b537d2eb93af5e76f3f
Khada-Shin <Aymen.Bahdoudi@esprit.tn>
2024-05-02 14:48:51 +0200

    mrigel

--- src/main/java/Utils/PDFGenerator.java
+++ src/main/java/Utils/PDFGenerator.java
@@ -3,7 +3,6 @@ package Utils;
 import javafx.event.ActionEvent;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
-import javafx.scene.control.cell.PropertyValueFactory;
 import org.apache.pdfbox.pdmodel.PDDocument;
 import org.apache.pdfbox.pdmodel.PDPage;
 import org.apache.pdfbox.pdmodel.PDPageContentStream;
@@ -12,6 +11,8 @@ import org.apache.pdfbox.pdmodel.font.PDType1Font;
 import javafx.scene.control.Alert;
 import javafx.scene.control.Alert.AlertType;
 import java.io.IOException;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 import javafx.scene.paint.Color;
 
 public class PDFGenerator {
@@ -23,12 +24,16 @@ public class PDFGenerator {
 
         try (PDPageContentStream contentStream = new PDPageContentStream(document, page)) {
 
+            // Draw table title "CompteClient Table" in blue
+            contentStream.setNonStrokingColor((float) Color.BLUE.getRed(), (float) Color.BLUE.getGreen(), (float) Color.BLUE.getBlue());
             contentStream.beginText();
             contentStream.setFont(PDType1Font.HELVETICA_BOLD, 16);
             contentStream.newLineAtOffset(200, 750);
             contentStream.showText(title);  // Use the provided title
             contentStream.endText();
 
+            // Draw table headers in black
+            contentStream.setNonStrokingColor(0, 0, 0);
             contentStream.beginText();
             contentStream.setFont(PDType1Font.HELVETICA_BOLD, 12);
             contentStream.newLineAtOffset(25, 700);
@@ -40,6 +45,9 @@ public class PDFGenerator {
             }
 
             contentStream.newLineAtOffset(-100 * tableView.getColumns().size(), -20); // Adjust Y offset
+
+            // Set font size for table data (even smaller)
+            contentStream.setFont(PDType1Font.HELVETICA, 6); // Set the font size to 6
 
             // Add table data
             for (Object item : tableView.getItems()) {
@@ -53,6 +61,17 @@ public class PDFGenerator {
             }
 
             contentStream.endText();
+
+            // Add date and time below the table
+            contentStream.beginText();
+            contentStream.setFont(PDType1Font.HELVETICA, 10);
+            contentStream.newLineAtOffset(200, 50);
+            LocalDateTime now = LocalDateTime.now();
+            DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
+            String formattedDateTime = now.format(formatter);
+            contentStream.showText("Date and Time: " + formattedDateTime);
+            contentStream.endText();
+
         } catch (IOException e) {
             e.printStackTrace();
         }
